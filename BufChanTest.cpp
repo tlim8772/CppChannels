cf6e2a8ffa122da820aac672da6e870154090a3a
obrona <tlim8772@gmail.com>
2025-11-20 15:32:23 +0800

    add multiple senders multiple receivers test

--- BufChanTest.cpp
+++ BufChanTest.cpp
@@ -93,7 +93,72 @@ void testSend2() {
     };
 }
 
+void testSend3() {
+    BufChan<int> chan{5};
+    vector<pair<int,bool>> send1, send2, recv1, recv2;
+
+    auto s1 = [&send1, &chan] () {
+        for (int i = 0; i < 100; i++) {
+            bool res = chan.send(i);
+            if (res) send1.emplace_back(i, res);
+        }
+    };
+
+    auto s2 = [&send2, &chan] () {
+        for (int i = 100; i < 200; i++) {
+            bool res = chan.send(i);
+            if (res) send2.emplace_back(i, res);
+        }
+    };
+
+    auto r1 = [&recv1, &chan] () {
+        for (int i = 0; i < 100; i++) {
+            auto [v, res] = chan.recv();
+            if (res) recv1.emplace_back(v, res);
+        }
+    };
+
+     auto r2 = [&recv2, &chan] () {
+        for (int i = 0; i < 100; i++) {
+            auto [v, res] = chan.recv();
+            if (res) recv2.emplace_back(v, res);
+        }
+    };
+
+
+
+    {
+        vector<jthread> ths;
+        ths.emplace_back(s1);
+        ths.emplace_back(s2);
+        ths.emplace_back(r1);
+        ths.emplace_back(r2);
+    }
+
+    for (auto [v, r] : send1) {
+        cout << "send " << v << " " << r << endl;
+    }
+    cout << endl;
+    for (auto [v, r] : send2) {
+        cout << "send " << v << " " << r << endl;
+    }
+    cout << endl;
+
+    sort(recv1.begin(), recv1.end(), [](pair<int,bool> p1, pair<int,bool> p2) { return p1.first < p2.first; });
+    for (auto [v, r] : recv1) {
+        cout << "recv " << v << " " << r << endl;
+    };
+    cout << endl;
+
+    sort(recv2.begin(), recv2.end(), [](pair<int,bool> p1, pair<int,bool> p2) { return p1.first < p2.first; });
+    for (auto [v, r] : recv2) {
+        cout << "recv " << v << " " << r << endl;
+    };
+    cout << endl;
+}
+
 int main() {
     //testSend1();
-    testSend2();
+    //testSend2();
+    testSend3();
 }
