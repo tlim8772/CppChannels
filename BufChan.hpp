1acc2b874b25f770ecfeafd13216a3ed2ad4e708
obrona <tlim8772@gmail.com>
2025-11-20 14:06:14 +0800

    (hopefully) completed BufChan

--- BufChan.hpp
+++ BufChan.hpp
@@ -7,7 +7,12 @@ when channel is closed, receiver must still be able to receive items inside the 
 for closing, suppose buffer size is 1, and have 99 senders that are blocking, it is possible
 that the closer thread inserts the ending val 1st.
 
-want to guarantee that if send(T t) returns true, a receiver can eventually receive t.
+guarantee that if send(T t) returns true, a receiver can eventually receive t, 
+if there are enough receivers.
+
+1 insight is that there can only be blocked senders or blocked receivers, never both.
+if send() notify one, it is always a blocked receiver.
+if recv() notify one, it is always a blocked sender.
 */
 
 template <typename T>
@@ -20,57 +25,48 @@ struct Ctrl {
 
 template <ValueInitializable T>
 struct BufChan {
-    const int buf_size;
-    atomic<Ctrl> ctrl;
-    atomic<int> s{0}, e{0};
+    bool closed = false;
+    int s = 0, e = 0, curr_size = 0;
     vector<T> buf;
+    mutex mut{};
+    condition_variable cv;
 
-    BufChan(int len): buf_size(len), ctrl{Ctrl{false, 0}}, buf(len, T()) {}
+    BufChan(int len): buf(len, T()) {}
 
     void close() {
-        Ctrl c = ctrl.load();
-        while (!ctrl.compare_exchange_strong(c, {true, c.len}));
+        mut.lock();
+        closed = true;
+        mut.unlock();
+        cv.notify_all();
     }
 
-    pair<T, bool> recv() {
-        Ctrl c;
-        start:
-            while (1) {
-                c = ctrl.load();
-                if (c.len == 0 && c.closed) return {T(), false};
-                if (c.len > 0) break;
-            }
-
-        while (!ctrl.compare_exchange_strong(c, {c.closed, c.len - 1})) {
-            if (c.len == 0 && c.closed) return {T(), false};
-            else if (c.len == 0) goto start;
+    pair<T,bool> recv() {
+        pair<T,bool> out;
+        {
+            unique_lock lk{mut};
+            if (closed) return {T(), false};
+            while (!closed && curr_size == 0) cv.wait(lk);
+            if (closed) return {T(), false};
+            curr_size--;
+            out = {buf[s], true};
+            s = (s + 1) % buf.size();
         }
-
-        int p = s.load();
-        while (!s.compare_exchange_strong(p, (p + 1) % buf_size));
-        return {buf[p], true};
+        cv.notify_one();
+        return out;
     }
 
     bool send(T t) {
-        Ctrl c;
-        start:
-            while (1) {
-                c = ctrl.load();
-                if (c.closed) return false;
-                if (c.len < buf_size) break;
-            }
-
-        while (!ctrl.compare_exchange_strong(c, {c.closed, c.len + 1})) {
-            if (c.closed) return false;
-            if (c.len == buf_size) goto start;
+        {
+            unique_lock lk{mut};
+            if (closed) return false;
+            while (!closed && curr_size == buf.size()) cv.wait(lk);
+            if (closed) return false;
+            curr_size++;
+            buf[e] = t;
+            e = (e + 1) % buf.size();
         }
-
-        int p = e.load();
-        while (!e.compare_exchange_strong(p, (p + 1) % buf_size));
-        buf[p] = std::move(t);
-
+        cv.notify_one();
         return true;
     }
-
 
 };
--- BufChanTest.cpp
+++ BufChanTest.cpp
@@ -2,23 +2,28 @@
 
 void testSend1() {
     BufChan<int> chan{5};
+
+    // store the results, here, so we can print once all the threads finish.
+    // to avoid output inteleaving.
     vector<pair<int,bool>> send_res, recv_res;
 
     auto t1 = [&chan, &send_res] () {
-        for (int i = 0; i < 5; i++) {
+        //this_thread::sleep_for(300ms);
+        for (int i = 0; i < 800; i++) {
             bool res = chan.send(i);
             send_res.emplace_back(i, res);
         }
     };
 
     auto t2 = [&chan, &recv_res] () {
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 1000; i++) {
             auto [v, res] = chan.recv();
             recv_res.emplace_back(v, res);
         }
     };
 
     auto t3 = [&chan] () {
+        this_thread::sleep_for(300ms);
         chan.close();
     };
 
